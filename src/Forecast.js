b9878c91b0845cf5ad405f4ee5e74728ce7d1e56
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2023-10-08 18:02:52 +0100

    first commit

---
+++ src/Forecast.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Forecast() {
+  let apiUrl = `api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid={API key}`;
+  return <div></div>;
+}
---
+++ src/WeatherSearch.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  // const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let time = new Date();
+
+  function displayWeather(response) {
+    // setLoaded(true);
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].main,
+      city: response.data.name
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <div class="input-group my-3 ">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter a city.."
+          onChange={updateCity}
+        />
+        {/* <span class="input-group-text bg-primary" id="basic-addon2"> */}
+        <button type="Submit" class="  btn-primary text-white border-none">
+          Search
+        </button>
+
+        <button type="submit" class=" btn-success text-white">
+          Current
+        </button>
+        {/* </span> */}
+      </div>
+    </form>
+  );
+
+  // if (loaded) {
+  return (
+    <div className="border border-secondary px-4 container" id="container">
+      <div className="d-flex ">
+        <a href="/">Lisbon</a>
+        <a href="/" className="px-4">
+          Paris
+        </a>
+        <a href="/">Sydney</a>
+        <a href="/" className="px-4">
+          SanFrancisco
+        </a>
+      </div>
+      {form}
+      <div className="">
+        <div className="text-capitalize city text-secondary">
+          {weather.city}
+        </div>
+        <div className="text-secondary">{weather.description}</div>
+        <div></div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex ">
+            <div>
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="d-flex">
+              <div>
+                <strong className=""> {Math.round(weather.temperature)}</strong>
+              </div>
+              <div>°C</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li className="text-secondary">
+              Precipitation: {weather.humidity}%
+            </li>
+            <li className="text-secondary">Wind: {weather.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+      {/* <ul>
+        <li>Temperature: {Math.round(weather.temperature)}°C</li>
+        <li>Description: {weather.description}</li>
+        <li>Humidity: {weather.humidity}%</li>
+        <li>Wind: {weather.wind}km/h</li>
+        <li>
+          <img src={weather.icon} alt={weather.description} />
+        </li>
+      </ul> */}
+    </div>
+  );
+  // } else {
+  //   return form;
+  // }
+}
